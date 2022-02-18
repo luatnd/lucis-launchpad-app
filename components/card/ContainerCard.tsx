e74fd5b8cc10348e1535076d52ea21f862809e1d
Dao882 <thedv@lucis.network>
2022-02-18 16:10:04 +0700

    add(card): add ui card

--- components/card/ContainerCard.tsx
+++ components/card/ContainerCard.tsx
@@ -1,11 +1,35 @@
-import s from './ContainerCard.module.sass'
+import s from './ContainerCard.module.sass';
+import GradientButton from '../Button/GradientButton';
+
 
 type Props = {
-
+    srcGame: string;
+    time: string;
+    nameGame: string;
+    title: string;
 };
 
-export default function CardContainer (props: Props) {
+export default function CardItem(props: Props) {
     return (
-        <h1>tao là card đây</h1>
+        <div className={s.CardContainer}>
+            <div className={s.img_game}>
+                <img src={props.srcGame} alt="" />
+            </div>
+            <div className={s.content}>
+                <div className={s.headingCard}>
+                    <div className={s.time}>{props.time}</div>
+                    <h5>{props.nameGame}</h5>
+                    <p>{props.title}</p>
+                </div>
+                <div className={s.btnDetail}><GradientButton type={1} className={s.styleBtn}>DETAIL</GradientButton></div>
+                <div className={s.groupIcon}>
+                    <a href="#"><img src="/assets/UpComing/win.svg" alt="" /></a>
+                    <a href="#"><img src="/assets/UpComing/fb.png" alt="" /></a>
+                    <a href="#"><img src="/assets/UpComing/dis.svg" alt="" /></a>
+                    <a href="#"><img src="/assets/UpComing/tele.svg" alt="" /></a>
+                    <a href="#"><img src="/assets/UpComing/tw.svg" alt="" /></a>
+                </div>
+            </div>
+        </div>
     )
 }
--- pages/UpComingPage/UpcomingCampaign.tsx
+++ pages/UpComingPage/UpcomingCampaign.tsx
@@ -1,12 +1,58 @@
-import CardContainer from 'components/card/ContainerCard'
+import CardItem from 'components/card/ContainerCard'
 import TitleSection from 'components/TitleNameSection'
 import s from './UpcomingCampaign.module.sass'
 
-export default function UpComing () {
-    return(
+
+type Props = {}
+
+const ListCard = [
+    {
+        srcGame: '/assets/UpComing/gameAxie.png',
+        time: '1d 05h 30m 25s',
+        nameGame: 'AXIE INFINITY',
+        title: 'Build up a collection and use them across an ever expanding universe of games',
+    },
+    {
+        srcGame: '/assets/UpComing/gameAxie.png',
+        time: '1d 05h 30m 25s',
+        nameGame: 'AXIE INFINITY',
+        title: 'Build up a collection and use them across an ever expanding universe of games',
+    },
+    {
+        srcGame: '/assets/UpComing/gameAxie.png',
+        time: '1d 05h 30m 25s',
+        nameGame: 'AXIE INFINITY',
+        title: 'Build up a collection and use them across an ever expanding universe of games',
+    },
+    {
+        srcGame: '/assets/UpComing/gameAxie.png',
+        time: '1d 05h 30m 25s',
+        nameGame: 'AXIE INFINITY',
+        title: 'Build up a collection and use them across an ever expanding universe of games',
+    },
+    {
+        srcGame: '/assets/UpComing/gameAxie.png',
+        time: '1d 05h 30m 25s',
+        nameGame: 'AXIE INFINITY',
+        title: 'Build up a collection and use them across an ever expanding universe of games',
+    },
+    {
+        srcGame: '/assets/UpComing/gameAxie.png',
+        time: '1d 05h 30m 25s',
+        nameGame: 'AXIE INFINITY',
+        title: 'Build up a collection and use them across an ever expanding universe of games',
+    },
+]
+
+export default function UpComing(props: Props) {
+    return (
         <section className='lucis-container'>
             <TitleSection text='Upcoming campaign' />
-            <CardContainer/>
+            <div className={s.blockCard}>
+                {ListCard.map((e, i) => (
+                    <CardItem srcGame={e.srcGame} time={e.time} nameGame={e.nameGame} title={e.title} />
+                ))}
+            </div>
         </section>
     )
 }

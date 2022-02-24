c839564d9562b865ab138534aa71b865959ddf23
cungbuituananh <cungbuituananh1@gmail.com>
2022-02-24 16:10:01 +0700

    Add feature  update fullname

--- pages/profile/ProfileInfo.tsx
+++ pages/profile/ProfileInfo.tsx
@@ -1,4 +1,6 @@
-import { useRef, useState } from "react";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import { useProfile } from "hooks/profile/useProfile";
+import { useEffect, useRef, useState } from "react";
 import s from "./index.module.sass";
 
 const userProfile = {
@@ -19,6 +21,7 @@ const Info = () => {
   const affilateIdRef = useRef<any>(null);
   const nameRef = useRef<any>(null);
   const [isEdit, setIsEdit] = useState(false);
+  const { updateFullName } = useProfile();
 
   const handleCopyAffilateId = () => {
     if (affilateIdRef) {
@@ -28,15 +31,27 @@ const Info = () => {
 
   const handleEditName = () => {
     setIsEdit(true);
-    if (nameRef) {
-      nameRef.current.focus();
-      console.log(nameRef.current);
-    }
   };
 
-  const handleBlur = () => {
+  const handleSubmitFullName = () => {
     setIsEdit(false);
+    updateFullName({ variables: { full_name: nameRef.current.innerText } });
   };
+
+  const handleCancelSubmitFullName = () => {
+    setIsEdit(false);
+    nameRef.current.innerText = userProfile.fullName;
+  };
+
+  useEffect(() => {
+    if (nameRef) {
+      nameRef.current.focus();
+      //@ts-ignore
+      document.execCommand("selectAll", false, null);
+      //@ts-ignore
+      document.getSelection().collapseToEnd();
+    }
+  }, [isEdit]);
 
   return (
     <div className={s.info}>
@@ -47,17 +62,23 @@ const Info = () => {
       <div>
         <div className={s.fullName}>
           <div className={s.name}>
-            <h1
-              onBlur={handleBlur}
-              ref={nameRef}
-              contentEditable={isEdit}
-              suppressContentEditableWarning={true}
-            >
+            <h1 ref={nameRef} contentEditable={isEdit} suppressContentEditableWarning={true}>
               {userProfile.fullName}
             </h1>
-            <button onClick={handleEditName}>
-              <img src="/assets/MyProfile/edit.svg" alt="" />
-            </button>
+            {isEdit ? (
+              <>
+                <button onClick={handleSubmitFullName}>
+                  <CheckOutlined />
+                </button>
+                <button onClick={handleCancelSubmitFullName}>
+                  <CloseOutlined />
+                </button>
+              </>
+            ) : (
+              <button onClick={handleEditName}>
+                <img src="/assets/MyProfile/edit.svg" alt="" />
+              </button>
+            )}
           </div>
 
           <p>{userProfile.id}</p>

fad35dbf7aedff464e6c4dbbb200fadfe17a7c21
loinv <loinv22@lucis.network>
2022-02-26 22:02:46 +0700

    Engine: update gitlab ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,4 @@
-# docker-compose up --build -d
-docker build -t lucis_launchpad:1.0 -f ./deploy/Dockerfile
-docker run --name lucis_lunchpad --network lucis_network --restart unless-stopped -p 3001:3001 lucis_launchpad
+git pull
+docker-compose up --build -d
+# docker build -t lucis_launchpad:1.0 -f ./deploy/Dockerfile .
+# docker run --name lucis_lunchpad --network lucis_network --restart unless-stopped -p 3001:3001 -d lucis_launchpad:latest
--- deploy/env.sh
+++ deploy/env.sh
@@ -1,2 +1,3 @@
 echo NEXT_PUBLIC_GRAPHQL_URL="https://lucis-lp.koolab.io/graphql" >> .env
+echo NEXT_PUBLIC_GRAPHQL_URL="https://lucis-lp.koolab.io/graphql" >> ../.env
 # echo NODE_ENV=$NODE_ENV >> .env

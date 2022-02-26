7c99225fd1100d55dbb5fde254551f69198ca59f
loinv <loinv22@lucis.network>
2022-02-26 22:24:10 +0700

    Engine: update git ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 git pull
 docker-compose up --build -d
-# docker build -t lucis_launchpad:1.0 -f ./deploy/Dockerfile .
-# docker run --name lucis_lunchpad --network lucis_network --restart unless-stopped -p 3001:3001 -d lucis_launchpad:latest
+# - docker build -t lucis_lunchpad_ci:$CI_COMMIT_SHORT_SHA -f ./deploy/Dockerfile .
+# - docker run --name lucis_lunchpad_ci --network lucis_network --restart unless-stopped -p 3001:3001 -d lucis_lunchpad_ci:$CI_COMMIT_SHORT_SHA

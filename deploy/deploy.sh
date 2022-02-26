77d3d7213cbac97a601fde7fae8d233c2906665d
loinv <loinv22@lucis.network>
2022-02-26 21:42:58 +0700

    Engine: docker config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,3 @@
-docker-compose up --build -d
+# docker-compose up --build -d
+docker build -t lucis_launchpad:1.0 -f ./deploy/Dockerfile
+docker run --name lucis_lunchpad --network lucis_network --restart unless-stopped -p 3001:3001 lucis_launchpad

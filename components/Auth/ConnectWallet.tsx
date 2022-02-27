fc19932ec939fc73870f7b9e8ed3c79680987cba
neo.mxn0 <neo.mxn0@gmail.com>
2022-02-27 21:48:15 +0700

    WalletConnect: UI fake select chain

--- components/Auth/ConnectWallet.tsx
+++ components/Auth/ConnectWallet.tsx
@@ -3,6 +3,7 @@ import { Modal, Button } from 'antd';
 
 import s from './ConnectWallet.module.sass';
 import GradientButton from '../Button/GradientButton';
+import {Chain, Wallet} from "../../utils/Chain";
 
 
 type Props = {
@@ -10,6 +11,8 @@ type Props = {
 };
 export default function Header(props: Props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [chain, setChain] = useState<Chain | null>(null);
+  const [wallet, setWallet] = useState<Wallet | null>(null);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -43,27 +46,27 @@ export default function Header(props: Props) {
       >
         <p className={s.title}>1. Choose network</p>
         <div className={s.items}>
-          <div className={s.item}>
+          <div onClick={() => setChain(Chain.eth)} className={`${s.item} ${chain === Chain.eth ? s.active : ''}`}>
             <img src="/assets/crypto/ico-chain-eth.svg" alt="" />
             <p>Ethereum</p>
           </div>
-          <div className={`${s.item} ${s.active}`}>
+          <div onClick={() => setChain(Chain.bsc)} className={`${s.item} ${chain === Chain.bsc ? s.active : ''}`}>
             <img src="/assets/crypto/ico-chain-bsc.png" alt="" />
             <p>BSC</p>
           </div>
-          <div className={`${s.item} ${s.disable}`}>
+          <div onClick={() => setChain(Chain.polygon)} className={`${s.item} ${chain === Chain.polygon ? s.active : ''}`}>
             <img src="/assets/crypto/ico-chain-polygon.png" alt="" />
             <p>Polygon</p>
           </div>
-          <div className={`${s.item} ${s.disable}`}>
+          <div className={`${s.item} ${s.disable} ${chain === Chain.near ? s.active : ''}`}>
             <img src="/assets/crypto/ico-chain-near.svg" alt="" />
             <p>NEAR</p>
           </div>
-          <div className={`${s.item} ${s.disable}`}>
+          <div className={`${s.item} ${s.disable} ${chain === Chain.flow ? s.active : ''}`}>
             <img src="/assets/crypto/ico-chain-flow.png" alt="" />
             <p>Flow</p>
           </div>
-          <div className={`${s.item} ${s.disable}`}>
+          <div className={`${s.item} ${s.disable} ${chain === Chain.avax ? s.active : ''}`}>
             <img src="/assets/crypto/ico-chain-avax.svg" alt="" />
             <p>Avalanche</p>
           </div>
@@ -72,17 +75,17 @@ export default function Header(props: Props) {
 
         <p className={s.title}>2. Choose wallet</p>
         <div className={s.items}>
-          <div className={s.item}>
+          <div onClick={() => setWallet(Wallet.metamask)} className={`${s.item} ${wallet === Wallet.metamask ? s.active : ''}`}>
             <img src="/assets/crypto/ico-wallet-metamask.png" alt="" />
             <p>Metamask</p>
           </div>
-          <div className={`${s.item} ${s.active}`}>
-            <img src="/assets/crypto/ico-chain-bsc.png" alt="" />
-            <p>BSC Wallet</p>
-          </div>
-          <div className={`${s.item} ${s.disable}`}>
+          <div onClick={() => setWallet(Wallet.wc)} className={`${s.item} ${wallet === Wallet.wc ? s.active : ''}`}>
             <img src="/assets/crypto/ico-wallet-wc.png" alt="" />
             <p>Wallet Connect</p>
+          </div>
+          <div className={`${s.item} ${s.disable} ${wallet === Wallet.bsc ? s.active : ''}`}>
+            <img src="/assets/crypto/ico-chain-bsc.png" alt="" />
+            <p>BSC Wallet</p>
           </div>
         </div>
       </Modal>
---
+++ utils/Chain.ts
@@ -0,0 +1,14 @@
+export enum Chain {
+  eth = 'eth',
+  bsc = 'bsc',
+  polygon = 'polygon',
+  near = 'near',
+  avax = 'avax',
+  flow = 'flow',
+}
+
+export enum Wallet {
+  metamask = 'metamask',
+  bsc = 'bsc',
+  wc = 'wc', // wallet connect
+}

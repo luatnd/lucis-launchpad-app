85793c81f5ca4e32abe4a52cac5fcd04f88e7633
Anh Tuấn Hoàng <macos@TuanAnhs-Macbook.local>
2022-03-02 15:55:39 +0700

    mapp

--- pages/DetailCampaign/components/CountDown/CountDown.tsx
+++ pages/DetailCampaign/components/CountDown/CountDown.tsx
@@ -1,55 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import s from './CountDown.module.sass'
+import React, { useEffect, useState } from "react";
+import s from "./CountDown.module.sass";
 
 const CountDown = () => {
-  const [totalTime, setTotalTime] = useState(20000)
-  const [timer, setTimer] = useState<{[name: string]: number}> ({ days: 0, hours: 0, minutes: 0, seconds: 0 })
-  
+  const [totalTime, setTotalTime] = useState(20000);
+  const [timer, setTimer] = useState<{ [name: string]: number }>({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
   useEffect(() => {
     setTimer((item) => ({
       ...item,
       days: Math.floor(totalTime / (60 * 60 * 24)),
       hours: Math.floor((totalTime / (60 * 60)) % 24),
-      minutes: Math.floor((totalTime / (60)) % 60),
-      seconds: Math.floor((totalTime % 60))
-    }))
-  }, [])
+      minutes: Math.floor((totalTime / 60) % 60),
+      seconds: Math.floor(totalTime % 60),
+    }));
+  }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     let interval: NodeJS.Timer;
-    interval = setInterval(()=> {
-      if(totalTime < 0) {
-        clearInterval(interval)
+    interval = setInterval(() => {
+      if (totalTime < 0) {
+        clearInterval(interval);
       } else {
-        countTime()
+        countTime();
       }
-    }, 1000)
-    return () => clearInterval(interval)
-  },[totalTime])
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [totalTime]);
 
   const countTime = () => {
-    if(totalTime > 0 && (timer.days !== 0 || timer.hours !== 0 || timer.minutes !== 0 || timer.seconds !==0 )) {
-      setTimer((item) => ({...item, seconds: item.seconds - 1}))
-      if( timer.minutes >= 0 && timer.seconds - 1 < 0) {
-        setTimer((item) => ({...item, seconds: 59}))
-        setTimer((item) => ({...item, minutes: item.minutes - 1}));
-        if(timer.hours >= 0 && timer.minutes - 1 < 0) {
-          setTimer((item) => ({...item, minutes: 59}));
-          setTimer((item) => ({...item, hours: item.hours - 1}));
-          if(timer.days >= 0 && timer.hours - 1 < 0) {
-            setTimer((item) => ({...item, hours: 23}));
-            if ( timer.days - 1 > 0) {
-              setTimer((item) => ({...item, days: item.days - 1}));
+    if (totalTime > 0 && (timer.days !== 0 || timer.hours !== 0 || timer.minutes !== 0 || timer.seconds !== 0)) {
+      setTimer((item) => ({ ...item, seconds: item.seconds - 1 }));
+      if (timer.minutes >= 0 && timer.seconds - 1 < 0) {
+        setTimer((item) => ({ ...item, seconds: 59 }));
+        setTimer((item) => ({ ...item, minutes: item.minutes - 1 }));
+        if (timer.hours >= 0 && timer.minutes - 1 < 0) {
+          setTimer((item) => ({ ...item, minutes: 59 }));
+          setTimer((item) => ({ ...item, hours: item.hours - 1 }));
+          if (timer.days >= 0 && timer.hours - 1 < 0) {
+            setTimer((item) => ({ ...item, hours: 23 }));
+            if (timer.days - 1 > 0) {
+              setTimer((item) => ({ ...item, days: item.days - 1 }));
             }
           }
         }
       }
     }
-    setTotalTime((totalTime) => totalTime - 1)
-  }
+    setTotalTime((totalTime) => totalTime - 1);
+  };
 
   return (
-    <div className='lucis-container'>
+    <div className={`${s.countDownContainer}`}>
       <div className='text-white text-center text-36px font-bold'>End to apply for the Whitelist in</div>
       <div className='flex text-white gap-10 justify-center mt-8'>
         {Object.keys(timer).map((item, key) => {
@@ -63,11 +63,11 @@ const CountDown = () => {
               </div>
               <div className='mt-3 uppercase font-bold text-center'>{item}</div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CountDown
+export default CountDown;
--- pages/DetailCampaign/components/SiteMap/SiteMap.tsx
+++ pages/DetailCampaign/components/SiteMap/SiteMap.tsx
@@ -1,79 +1,93 @@
-import React, { createRef, useEffect, useRef } from 'react'
-import Swiper from 'swiper';
-import s from './SiteMap.module.sass';
+import React, { createRef, useEffect, useRef } from "react";
+import Swiper from "swiper";
+import s from "./SiteMap.module.sass";
 const ListCard = [
   {
-    title: 'Upcoming',
-    time: '08:00, Feb 18',
-    description: 'Stay tuned and prepare to APPLY WHITELIST.',
-    isActive: false
+    title: "Upcoming",
+    time: "08:00, Feb 18",
+    description: "Stay tuned and prepare to APPLY WHITELIST.",
+    isActive: false,
   },
   {
-    title: 'Register whitelist',
-    time: '08:00, Feb 18',
-    description: 'Click the [APPLY WHITELIST] button to register for Register whitelist.',
-    isActive: true
+    title: "Register whitelist",
+    time: "08:00, Feb 18",
+    description: "Click the [APPLY WHITELIST] button to register for Register whitelist.",
+    isActive: true,
   },
   {
-    title: 'Whitelist phase',
-    time: '08:00, Feb 18',
-    description: 'Whitelist registrants will be given favorable dealings to buy Mystery Boxes in phase 1, on a FCFS basis.',
-    isActive: false
+    title: "Whitelist phase",
+    time: "08:00, Feb 18",
+    description:
+      "Whitelist registrants will be given favorable dealings to buy Mystery Boxes in phase 1, on a FCFS basis.",
+    isActive: false,
   },
   {
-    title: 'Buy phase',
-    time: '08:00, Feb 18',
-    description: 'The Buy phase will be started right after whitelist phase ends. Remaining boxes left in whitelist phase will be transferred to Buy phase',
-    isActive: false
+    title: "Buy phase",
+    time: "08:00, Feb 18",
+    description:
+      "The Buy phase will be started right after whitelist phase ends. Remaining boxes left in whitelist phase will be transferred to Buy phase",
+    isActive: false,
   },
   {
-    title: 'Close',
-    time: '08:00, Feb 18',
-    description: 'Thank you.',
-    isActive: false
-  }
+    title: "Close",
+    time: "08:00, Feb 18",
+    description: "Thank you.",
+    isActive: false,
+  },
 ];
 
 const SiteMap = () => {
-  const SwiperRef = useRef<Swiper>()
-  useEffect(()=> {
-    SwiperRef.current = new Swiper('.swiper-container', {
+  const SwiperRef = useRef<Swiper>();
+  useEffect(() => {
+    SwiperRef.current = new Swiper(".swiper-container", {
       slidesPerView: 5,
       navigation: {
-        'prevEl': 'prev-slide',
-        'nextEl': 'next-slide'
-      }
-    })
-    console.log(SwiperRef.current.activeIndex)
-  },[])
+        prevEl: "prev-slide",
+        nextEl: "next-slide",
+      },
+    });
+    console.log(SwiperRef.current.activeIndex);
+  }, []);
 
   return (
-    <div className='flex justify-center relative'>
+    <div className={`flex justify-center relative ${s.SiteMapContainer}`}>
       {/* <button className="prev-slide">Prev</button> */}
       {/* <button className="next-slide">Next</button> */}
       <div className={`${s.SiteMapLineTimeLine}`}></div>
       <div className={`w-10/12`}>
-        <div className="swiper-container overflow-hidden">
-          <div className="swiper-wrapper">
+        <div className='swiper-container overflow-hidden'>
+          <div className='swiper-wrapper'>
             {ListCard.map((item, key) => {
               return (
-                  <div className="swiper-slide" key={key}>
-                    <div className={`flex flex-col justify-center select-none px-2 ${s.SiteMapLineCircleTitleBox}`}>
-                      <div className={`h-[250px] flex flex-col justify-end w-full`}>
-                        <div className={`text-white font-bold ${s.SiteMapLineCircleTitle} ${item.isActive === true ? s.active : ''}`}>{item.title}</div>
-                        <div className={`text-white pb-2 mb-10 ${s.SiteMapLineCircleTime} ${item.isActive === true ? s.active : ''}`}>{item.time}</div>
+                <div className='swiper-slide' key={key}>
+                  <div className={`flex flex-col justify-center select-none px-2 ${s.SiteMapLineCircleTitleBox}`}>
+                    <div className={`h-[250px] flex flex-col justify-end w-full`}>
+                      <div
+                        className={`text-white font-bold ${s.SiteMapLineCircleTitle} ${
+                          item.isActive === true ? s.active : ""
+                        }`}
+                      >
+                        {item.title}
                       </div>
-                      <div className={`${s.SiteMapLineCircle} ${item.isActive === true ? s.active : ''}`}></div>
-                      <div className={`text-white mt-10 w-full ${s.SiteMapLineCircleContent}`}>{item.description}</div>
+                      <div
+                        className={`text-white pb-2 mb-10 ${s.SiteMapLineCircleTime} ${
+                          item.isActive === true ? s.active : ""
+                        }`}
+                      >
+                        {item.time}
+                      </div>
                     </div>
+                    <div className={`${s.SiteMapLineCircle} ${item.isActive === true ? s.active : ""}`}></div>
+                    <div className={`text-white mt-10 w-full ${s.SiteMapLineCircleContent}`}>{item.description}</div>
                   </div>
-              )
+                </div>
+              );
             })}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SiteMap
+export default SiteMap;

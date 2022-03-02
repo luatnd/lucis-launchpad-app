9e365aeb1b35e78461dda85abc6b6cce85f0a88f
Anh Tuấn Hoàng <macos@TuanAnhs-Macbook.local>
2022-03-02 13:54:22 +0700

    bannẻ

--- pages/DetailCampaign/index.tsx
+++ pages/DetailCampaign/index.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import CountDown from './components/CountDown/CountDown';
-import SiteMap from './components/SiteMap/SiteMap'
+import React from "react";
+import CountDown from "./components/CountDown/CountDown";
+import SiteMap from "./components/SiteMap/SiteMap";
 import Team from "./components/Team/Team";
-import {Tabs} from "antd";
-import {TabPane} from "rc-tabs";
+import { Tabs } from "antd";
+import { TabPane } from "rc-tabs";
 import Trailer from "./components/Trailer/Trailer";
 import Banner from "./components/Banner/Banner";
+import s from "./detail.module.sass";
 
 function DetailCampaign() {
-    return (
-        <div className='lucis-container'>
-            <Banner />
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="TIMELINE" key="1">
-                    <SiteMap />
-                    <CountDown />
-                </TabPane>
-                <TabPane tab="RULE" key="2">
-                    hello rule
-                </TabPane>
-                <TabPane tab="ABOUT PROJECT" key="3">
-                    <Trailer />
-                    <Team />
-                </TabPane>
-            </Tabs>
-        </div>
-    );
+  return (
+    <div className='lucis-container'>
+      <div className={s.containerApp}>
+        <Banner />
+        <Tabs defaultActiveKey='1'>
+          <TabPane tab='TIMELINE' key='1'>
+            <SiteMap />
+            <CountDown />
+          </TabPane>
+          <TabPane tab='RULE' key='2'>
+            hello rule
+          </TabPane>
+          <TabPane tab='ABOUT PROJECT' key='3'>
+            <Trailer />
+            <Team />
+          </TabPane>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export default DetailCampaign;

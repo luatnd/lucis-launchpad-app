7dd688a7a34890b9209c19b6410a791d65a3d9aa
cungbuituananh <cungbuituananh1@gmail.com>
2022-03-07 18:30:25 +0700

    Add padding input in profile

--- components/Profile/ProfileInfo.tsx
+++ components/Profile/ProfileInfo.tsx
@@ -83,7 +83,7 @@ const Info = ({ isEdit, setIsEdit, profile }: Props) => {
           <div className={s.info}>
             <div>
               {isEdit ? (
-                <Input {...props} />
+                <Input {...props} placeholder={"Your name"} />
               ) : (
                 // <input className={s.name} onChange={handleChangeName} value={tempName} />
                 <p className={s.name}>{tempName}</p>

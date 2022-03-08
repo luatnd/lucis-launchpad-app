b234e5d08516dcf421a4db036a4766b3ea2f4961
luatnd_lucis <luatnd@lucis.network>
2022-03-08 15:21:50 +0700

    BoxCampaign: Update debug screen

--- components/campaign/components/CampaignBox/index.tsx
+++ components/campaign/components/CampaignBox/index.tsx
@@ -1,69 +1,89 @@
 import React from 'react';
 import { Button, Form, Input, InputNumber, Progress } from "antd";
 import s from '../Box/Box.module.sass'
+import { BoxType } from "src/generated/graphql";
+import { getCurrentCampaignRound } from "../../CampaignHelper";
 
+type Props = {
+  box: BoxType,
+}
+const CampaignBox = (props: Props) => {
+  const {box} = props;
 
-const CampaignBox = () => {
-  const e = {
-    name: 'LEGENDARY BOX',
-    image: '/assets/Box/image110.png',
-    description: '',
-    d1: [
-      {name: '250 rare NFTs', number: '50%'},
-      {name: '150 elite NFTs', number: '20%'},
-      {name: '75 legendary NFTs', number: '25%'},
-      {name: 'Network', number: 'BSC'}
-    ]
-  }
+  const currentRound = getCurrentCampaignRound(box.campaign);
+  const requireWhitelist = currentRound.require_whitelist;
+
+  /**
+   * In the future, we'll support selling box in multi currency (multi-chain) for 1 campaign
+   * This is phase 1: We support selling box in 1 currency only in 1 campaign
+   */
+  const firstBoxPrice = box.prices![0];
+
+  // TODO:
+  const sold_amount = 60;
+  const total_amount = 100;
+
+  const userIsWhitelisted = true; // TODO:
+  const userCanBuy = (requireWhitelist ? userIsWhitelisted : true)
+    && (sold_amount < total_amount)
+  ; // whitelist(if needed) && has not sold out
+  const firstBoxPrice_CurrencyIcon = '/assets/Box/image125.png'; // TODO:
 
   return (
     <div>
       <div className='flex justify-center'>
-        <h3 className='uppercase text-center text-white font-bold text-[24px] md:text-[36px]'>{e.name}</h3>
+        <h3 className='uppercase text-center text-white font-bold text-[24px] md:text-[36px]'>{box.name}</h3>
       </div>
       <div className={`mt-5 h-[798px] ${s.boxDetail} relative`}>
         <div className={`${s.bgImage}`}>
-          <img src={e.image} width='340px' height='273px' className='mx-auto block pt-[60px]' alt="" />
+          <img src={box.thumb_img ?? ''} width='340px' height='273px' className='mx-auto block pt-[60px]' alt="" />
         </div>
         <div className={`max-w-[340px] mx-auto absolute ${s.boxMain}`}>
-          {e.description && <div className='flex gap-2'>
+          {<div className='flex gap-2'>
             <div>
-              <div className={s.boxDes}></div>
+              <div className={s.boxDes}/>
             </div>
-            <p className='text-white text-18px'>{e.description}</p>
+            <p className='text-white text-18px'>TODO: Common box includes 5 upgradable NFT monsters that can be used in all game modes</p>
           </div>}
-          {e.d1.map((f, index) => (
-            <div key={index} className='flex justify-between text-white font-bold text-24px mb-2'>
-              <span>{f.name}</span>
-              <span>{f.number}</span>
-            </div>
-          ))}
-          {!e.description &&
-          <Form>
+
+          {/*
+          box.series_content must be html table in the database
+          */}
+          <div className={s.seriesContent} dangerouslySetInnerHTML={{__html: box.series_content ?? ''}} />
+
+          {/*{e.d1.map((f, index) => (*/}
+          {/*  <div key={index} className='flex justify-between text-white font-bold text-24px mb-2'>*/}
+          {/*    <span>{f.name}</span>*/}
+          {/*    <span>{f.number}</span>*/}
+          {/*  </div>*/}
+          {/*))}*/}
+
+
+
+          {userCanBuy && <Form className={s.buyForm}>
             <div className='flex justify-between text-white font-bold text-24px mb-2'>
               <Form.Item>
                 <label className={s.label}>Amount: </label>
                 <InputNumber />
               </Form.Item>
-              <span>Max:100</span>
+              <span>Max: {box.limit_per_user}</span>
             </div>
             <div className='flex justify-between text-white items-center font-bold text-24px mb-2'>
               <Button className={s.submit}>BUY</Button>
-              <span>Whitelist only</span>
+              {requireWhitelist && <span>Whitelist only</span>}
             </div>
-          </Form>
+          </Form>}
 
-          }
           <div className='font-bold text-white text-18px mt-[29px]'>
             <div className='flex justify-between items-center'>
               <span>Price per 1 box:</span>
               <div className='flex items-center gap-1'>
-                <img src="/assets/Box/image125.png" width="40px" height="40px" alt="" />
-                <span>16.79 BUSD</span>
+                <img src={firstBoxPrice_CurrencyIcon} width="40px" height="40px" alt="" />
+                <span>{firstBoxPrice.price} {firstBoxPrice.currency.symbol}</span>
               </div>
             </div>
-            <Progress showInfo={false} />
-            <p className='text-right'>0/100boxes</p>
+            <Progress showInfo={false} percent={Math.floor(sold_amount / total_amount * 100)} />
+            <p className='text-right'>{`${sold_amount}/${total_amount}`} boxes</p>
           </div>
         </div>
       </div>
--- pages/campaign/debug.tsx
+++ pages/campaign/debug.tsx
@@ -7,6 +7,7 @@ import { TabPane } from "rc-tabs";
 import Footer from "../../components/Footer";
 import React from "react";
 import CampaignBox from "../../components/campaign/components/CampaignBox";
+import { BoxCampaign, BoxType, Chain, Game } from "../../src/generated/graphql";
 
 /**
  * This has no function in project
@@ -15,6 +16,98 @@ import CampaignBox from "../../components/campaign/components/CampaignBox";
  * Match route: /campaign
  */
 const CampaignDebug: NextPage = () => {
+  // const chain: Chain = {
+  //   // _count: {
+  //   //   boxPrices: 0,
+  //   //   currencies: 0,
+  //   //   nftBox: 0,
+  //   // },
+  //   name: undefined,
+  //   symbol: "",
+  //   created_at: undefined,
+  //   updated_at: undefined,
+  // }
+  // @ts-ignore
+  const game: Game = {
+    uid: "",
+    name: "Thetan Arena",
+    created_at: undefined,
+    updated_at: undefined,
+    // _count: {
+    //   boxCampaigns: 0
+    // },
+  }
+
+  // @ts-ignore
+  const campaign: BoxCampaign = {
+    game: game,
+    game_uid: "",
+    uid: "cp_12a34f56b89",
+    start: new Date(),
+    end: new Date(),
+    created_at: undefined,
+    updated_at: undefined,
+    // _count: {
+    //   boxTypes: 0,
+    //   buyHistory: 0,
+    //   whitelists: 0,
+    // },
+  }
+
+  const box1: BoxType = {
+    // _count: {
+    //   prices: 0
+    // },
+    uid: "box1_123456789",
+    name: "Test campaign 1",
+    box_campaign_uid: "cp_12a34f56b89",
+    campaign: campaign,
+    limit_per_user: 100,
+    prices: [
+      {
+        chain_symbol: "BSC",
+        // @ts-ignore
+        currency: {
+          symbol: "BUSD",
+        },
+      }
+    ],
+    series_content: `
+    <table>
+      <tr>
+          <td>250 rare NFTs</td>
+          <td>50%</td>
+      </tr>
+      <tr>
+          <td>150 elite NFTs</td>
+          <td>25%</td>
+      </tr>
+      <tr>
+          <td>75 legendary NFTs</td>
+          <td>20%</td>
+      </tr>
+    </table>
+    `,
+    sold_amount: 380,
+    total_amount: 500,
+    thumb_img: '/assets/Box/image107.png',
+    created_at: undefined,
+    updated_at: undefined,
+  }
+  const box2: BoxType = {
+    ...box1,
+    uid: "box1_123456789_2",
+    name: "Test campaign 2",
+    thumb_img: '/assets/Box/image109.png',
+  }
+  const box3: BoxType = {
+    ...box1,
+    uid: "box1_123456789_3",
+    name: "Test campaign 3",
+    thumb_img: '/assets/Box/image110.png',
+  }
+
+
   return (
     <>
       <DocHead title={"Draft Page for campaign"}/>
@@ -22,16 +115,15 @@ const CampaignDebug: NextPage = () => {
         <div className={s.containerApp} style={{paddingTop: 150}}>
           <Row gutter={[24, 50]} className='justify-center'>
             <Col>
-              <CampaignBox/>
+              <CampaignBox box={box1} />
             </Col>
             <Col>
-              <CampaignBox/>
+              <CampaignBox box={box2} />
             </Col>
             <Col>
-              <CampaignBox/>
+              <CampaignBox box={box3} />
             </Col>
           </Row>
-
           <Footer/>
         </div>
       </div>

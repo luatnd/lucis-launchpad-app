aa2d9933b278b30560c0ea1d4f4f64c81366407c
anhcbt <anhcbt@lucis.network>
2022-03-11 09:54:48 +0700

    Create INO page

--- components/Ino/components/GotProject/GotProject.tsx
+++ components/Ino/components/GotProject/GotProject.tsx
@@ -2,17 +2,19 @@ import s from "./GotProject.module.sass";
 
 const GotProject = () => {
   return (
-    <>
-      <p>Got a Project?</p>
+    <div className={s.projectContainer}>
+      <p className="text-center text-[16px] md:text-[26px] lg:text-[36px] font-[600]">
+        Got a Project?
+      </p>
       <div className={s.line}></div>
-      <p>
+      <p className="text-center text-[14px] md:text-[16px]">
         Apply for an INO on Lucis Launchpad, submit your project and get a response within 24 hours
       </p>
-      <div className={s.buttonContainer}>
-        <button>Apply Now</button>
-        <button>Contact Us</button>
+      <div className={`${s.buttonContainer}`}>
+        <button className={s.applyBtn}>Apply Now</button>
+        <button className={s.contactBtn}>Contact Us</button>
       </div>
-    </>
+    </div>
   );
 };
 

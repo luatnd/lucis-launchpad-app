9497aa7f7f2f433b88d614501467961cd0031ac1
anhcbt <anhcbt@lucis.network>
2022-03-24 15:53:33 +0700

    Add blankstate for upcoming & opening

--- components/Home/UpComingPage/UpcomingCampaign.tsx
+++ components/Home/UpComingPage/UpcomingCampaign.tsx
@@ -1,4 +1,5 @@
 import { Col, message, Row } from "antd";
+import BlankState from "components/BlankState/BlankState";
 import { calculateCampaignStatus } from "components/campaign/CampaignHelper";
 import CardItem from "components/card/ContainerCard";
 import TitleSection from "components/TitleNameSection";
@@ -17,38 +18,42 @@ export default function UpComing(props: Props) {
   return (
     <section className="lucis-container">
       <TitleSection text="Upcoming campaign" />
-      <Row gutter={[30, 30]}>
-        {resultUpComing?.upcomingBoxCampaign.map(
-          (e: GBoxCampaign, index: number) => {
-            const statusTime = calculateCampaignStatus(e);
-            // console.log(statusTime);
+      {resultUpComing?.upcomingBoxCampaign.length > 0 ? (
+        <Row gutter={[30, 30]}>
+          {resultUpComing?.upcomingBoxCampaign.map(
+            (e: GBoxCampaign, index: number) => {
+              const statusTime = calculateCampaignStatus(e);
+              // console.log(statusTime);
 
-            return (
-              <Col key={index} xs={24} md={12} lg={8}>
-                <CardItem
-                  key={index}
-                  srcGame={e.cover_img}
-                  statusTime={statusTime}
-                  time={e.opening_at}
-                  // inTime={e.inTime}
-                  nameGame={e?.game.name}
-                  styleBg={true}
-                  title={e?.name}
-                  description={e?.game.desc}
-                  srcWeb={e?.game.website}
-                  srcFb={e?.game.facebook}
-                  srcTele={e?.game.telegram}
-                  srcDiscord={e?.game.discord}
-                  srcTwitter={e?.game.twitter}
-                  id={e?.uid}
-                  highlight={e?.highlight}
-                  chains={e?.chains}
-                />
-              </Col>
-            );
-          }
-        )}
-      </Row>
+              return (
+                <Col key={index} xs={24} md={12} lg={8}>
+                  <CardItem
+                    key={index}
+                    srcGame={e.cover_img}
+                    statusTime={statusTime}
+                    time={e.opening_at}
+                    // inTime={e.inTime}
+                    nameGame={e?.game.name}
+                    styleBg={true}
+                    title={e?.name}
+                    description={e?.game.desc}
+                    srcWeb={e?.game.website}
+                    srcFb={e?.game.facebook}
+                    srcTele={e?.game.telegram}
+                    srcDiscord={e?.game.discord}
+                    srcTwitter={e?.game.twitter}
+                    id={e?.uid}
+                    highlight={e?.highlight}
+                    chains={e?.chains}
+                  />
+                </Col>
+              );
+            }
+          )}
+        </Row>
+      ) : (
+        <BlankState title={"upcoming"} />
+      )}
     </section>
   );
 }
